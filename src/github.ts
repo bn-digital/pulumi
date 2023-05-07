86d96383bcef51cd33dde9c049c60ab99984ebe0
Vlad Volkov <vlad.volkov@bndigital.co>
2023-05-08 02:52:10 +0300

    Added reusable workflow

--- src/github.ts
+++ src/github.ts
@@ -1,7 +1,7 @@
 import { ActionsSecret } from "@pulumi/github"
-import { Config,type Output  } from "@pulumi/pulumi"
+import { Config, type Output } from "@pulumi/pulumi"
 const config = new Config()
-function createSecret({ key: secretName, value: plaintextValue }: { key: string; value: string|Output <string> }) {
+function createSecret({ key: secretName, value: plaintextValue }: { key: string; value: string | Output<string> }) {
   return new ActionsSecret(secretName?.toString().toLowerCase().replace(/_/g, "-"), {
     secretName,
     repository: config.name,
--- src/vault.ts
+++ src/vault.ts
@@ -1,18 +1,26 @@
+import { ActionsSecret } from "@pulumi/github"
 import { Output } from "@pulumi/pulumi"
 import { kv } from "@pulumi/vault"
 import { createSecret } from "./github"
 
+export interface VaultSecret {
+  data: { [key: string]: string }
+  metadata: unknown
+}
+
 function getSecret(path: string): Output<string> {
-  const vaultSecretKey = path.split("/").pop() as string
-  return kv.getSecretOutput({ path }).dataJson.apply(json => `${JSON.parse(json)?.[vaultSecretKey]}`)
+  const chunks = path.split("/")
+  const vaultSecretKey = chunks.pop()
+  const output = kv.getSecretOutput({ path: chunks.join("/") }).dataJson.apply(JSON.parse) as Output<VaultSecret>
+  return output.apply(it => (vaultSecretKey ? it?.data?.[vaultSecretKey] : ""))
 }
 
 /**
  * @param key GitHub secret name
  * @param path Vault secret path
  */
-function copyToGithubSecrets(key: string, path: string) {
-  return createSecret({ key, value: getSecret(path) })
+function copyToGithubSecrets(key: string, path: string): Output<ActionsSecret> {
+  return getSecret(path).apply(value => createSecret({ key, value }))
 }
 
 export { copyToGithubSecrets, getSecret }

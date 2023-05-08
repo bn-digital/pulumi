eda8cf9aeff251b7dff47758484d7b8f61712103
Vlad Volkov <vlad.volkov@bndigital.co>
2023-05-08 14:28:32 +0300

    Added github var processing

--- src/github.ts
+++ src/github.ts
@@ -1,11 +1,23 @@
-import { ActionsSecret } from "@pulumi/github"
+import { ActionsSecret, ActionsVariable } from "@pulumi/github"
 import { Config, type Output } from "@pulumi/pulumi"
+
 const config = new Config()
+const name = config.name
+
 function createSecret({ key: secretName, value: plaintextValue }: { key: string; value: string | Output<string> }) {
   return new ActionsSecret(secretName?.toString().toLowerCase().replace(/_/g, "-"), {
     secretName,
-    repository: config.name,
+    repository: name,
     plaintextValue,
   })
 }
-export { createSecret }
+
+function createVariable({ key: variableName, value: value }: { key: string; value: string | Output<string> }) {
+  return new ActionsVariable(variableName?.toString().toLowerCase().replace(/_/g, "-"), {
+    variableName,
+    repository: name,
+    value,
+  })
+}
+
+export { createSecret, createVariable }
